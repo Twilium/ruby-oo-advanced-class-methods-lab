a1377f968fb61fe6ea657dba618e25928d0251cb
Emmanuel <rivera128@live.com>
2020-11-15 13:05:49 -0600

    commit tree 1e5d199ccb6e7dd7bb579fd9fa1cd5563698191f

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,62 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    song = self.all.detect {|song| song.name == name}
+    song
+  end
+
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    if song
+      song
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    alphabetical_sort = self.all.sort_by {|song| song.name}
+    alphabetical_sort
+  end
+
+  def self.new_from_filename(filename)
+    songs = filename.split(" - ")
+    songs[1] = songs[1].chomp(".mp3")
+    song = self.new
+    song.name = songs[1]
+    song.artist_name = songs[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    results = self.new_from_filename(filename)
+    song = self.create
+    song.name = results.name
+    song.artist_name = results.artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
